39fb9d253e04e435a5ac422be10b159618d58237
Maitreya Shelare <maitreya.cse@gmail.com>
2023-01-14 22:18:08 +0530

    Added Bootstrap and Icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+// import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 Vue.config.productionTip = false;
 
