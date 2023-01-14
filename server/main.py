0756afd75ec683e412239229cb90784731e5ee02
Maitreya Shelare <maitreya.cse@gmail.com>
2023-01-14 21:55:58 +0530

    Added Server Folder

---
+++ server/main.py
@@ -0,0 +1,11 @@
+# from __init__ import create_app
+
+# app = create_app()
+
+# @app.route('/')
+# def index():
+#     return 'Backend'
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+print("Hello")
