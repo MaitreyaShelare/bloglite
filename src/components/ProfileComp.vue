a1013a6e8ec3fd6e15885cf355652cbcf911a211
Maitreya Shelare <maitreya.cse@gmail.com>
2023-03-25 12:56:52 +0530

    RETURN HERE< CHANGING DB

--- src/components/ProfileComp.vue
+++ src/components/ProfileComp.vue
@@ -123,7 +123,7 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           this.profileData = data;
-          // console.log(data);
+          console.log(data);
           // console.log(this.profileData.name);
           // var dp = document.getElementById("user-dp-image");
           // dp.src = `data:${data.dp_mimetype};charset=utf-8;base64,${data.dp}`;
