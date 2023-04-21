7b085ae87220f5966a1ed648d58bc94653a87104
Maitreya Shelare <maitreya.cse@gmail.com>
2023-04-21 16:59:31 +0530

    working

--- server/report.py
+++ server/report.py
@@ -1,15 +1,16 @@
 from jinja2 import Template
 from weasyprint import HTML
 
-def format_report(template_file, data):
-    with open(template_file, 'r') as f:
+def format_report(template_file, data={}):
+    with open(template_file) as f:
         template = Template(f.read())
-    return template.render(data)
+    return template.render(data=data)
 
 def generate_report(data):
-    message = format_report("/home/tej/Documents/MAD-II/bloglite/server/report.html", data=data)
+    # message = format_report("/home/tej/Documents/MAD-II/bloglite/server/report.html", data)
+    message = format_report("bloglite/server/report.html", data=data)
     html=HTML(string=message)
-    file_name = "report.pdf"
+    file_name = str(data['name']) + ".pdf"
     html.write_pdf(target=file_name)
 
 def main():
